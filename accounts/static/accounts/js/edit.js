66fe2acc73f33fbd4e941e918ee94bc296a43fb0
yhkim337 <yhkim4433@gmail.com>
2022-07-22 07:35:18 +0900

    [feat] subscribe&edit change

--- accounts/static/accounts/js/edit.js
+++ accounts/static/accounts/js/edit.js
@@ -1,44 +1,30 @@
-(() => {
-    selectedControl();
-  })();
+// (() => {
+//     selectedControl();
+//   })();
 
-function selectedControl(){
-    const el = document.getElementById('army_type');
-    const len = el.options.length; 
-    const str = document.getElementById('searchValue').value;
-    for (let i=0; i<len; i++){  
-      if(el.options[i].value == str){
-          el.options[i].selected = true;
-      }
-    }  
-  }
-
-// const countCheck = (obj) => {
-//   var chkBox = document.getElementsByName("chk");
-//   var chkCnt = 0;
-//   for(var i = 0; i< chkBox.length; i++){
-//     if(chkBox[i].checked){
-//       chkCnt++;
-//     }
+// function selectedControl(){
+//     const el = document.getElementById('army_type');
+//     const len = el.options.length; 
+//     const str = document.getElementById('searchValue').value;
+//     for (let i=0; i<len; i++){  
+//       if(el.options[i].value == str){
+//           el.options[i].selected = true;
+//       }
+//     }  
 //   }
-//   if(chkCnt > 2){
-//     alert("최대 2개까지 선택 가능합니다.");
-//     obj.checked = false;
-//     return false;
-//   }
-// }
 
-const CountChecked = (field) => {
-  var maxChecked = 2;   //선택가능한 체크박스 갯수
-  var totalChecked = 0; // 설정 끝
-    if (field.checked) // input check 박스가 체크되면 
-        totalChecked += 1; // totalChecked 증가
-    else // 체크가 아니면
-        totalChecked -= 1; // totalChecked 감소
+const showfield1 = (name) => {
+  if(name=='주식'){
+      document.getElementById('div1').innerHTML='주식종목: <input type="text" name="stock_type" />';
+  } else{
+      document.getElementById('div1').innerHTML='';
+  } 
+};
 
-    if (totalChecked > maxChecked) { // totalchecked 수가 maxchecked수보다 크다면
-        alert ("최대 2개 까지만 가능합니다."); // 팝업창 띄움
-    field.checked = false;
-    totalChecked -= 1;
-    }
-}
+const showfield2 = (name) => {
+  if(name=='주식'){
+      document.getElementById('div2').innerHTML='주식종목: <input type="text" name="stock_type" />';
+  } else{
+      document.getElementById('div2').innerHTML='';
+  } 
+};
