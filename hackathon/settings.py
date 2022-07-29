95004600a75632428f4721372bfdcf54226d90dd
yhkim337 <yhkim4433@gmail.com>
2022-07-29 18:25:08 +0900

    heroku

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'soldierLetter', 'static'),
-    os.path.join(BASE_DIR, 'accounts', 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'soldierLetter', 'static'),
+#     os.path.join(BASE_DIR, 'accounts', 'static'),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
