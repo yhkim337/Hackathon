448fab865326fbcf814a1578d1f2e5aaa0db87cc
yhkim337 <yhkim4433@gmail.com>
2022-07-29 17:19:33 +0900

    heroku

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -120,19 +120,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles' 
-# STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'soldierLetter', 'static'),
-#     os.path.join(BASE_DIR, 'accounts', 'static'),
-    
-# )
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'soldierLetter', 'static'),
+    os.path.join(BASE_DIR, 'accounts', 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
